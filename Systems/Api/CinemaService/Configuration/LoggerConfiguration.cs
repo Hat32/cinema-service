a98ff4503b5b80ec5cf7bf456033e60650d43614
Roman Vetrov <ash.tree4314@gmail.com>
2023-04-14 22:26:30 +0300

    Logger configuration

---
+++ Systems/Api/CinemaService/Configuration/LoggerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace CinemaService.Api.Configuration;
+using Serilog;
+
+
+/// <summary>
+///  Logger Configuration
+/// </summary>
+public static class LoggerConfiguration
+{
+    public static void AddAppLogger(this WebApplicationBuilder builder)
+    {
+        /// <summary>
+        /// Add logger 
+        /// </summary>
+        var logger = new Serilog.LoggerConfiguration()
+            .Enrich.WithCorrelationIdHeader()
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+
+        builder.Host.UseSerilog(logger, true);
+    }
+
+}
+
