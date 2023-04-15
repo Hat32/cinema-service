4163d318fdfcd8f5b8dd4b464a58f049f712eb29
Roman Vetrov <ash.tree4314@gmail.com>
2023-04-15 17:13:07 +0300

    Add versioning

---
+++ Systems/Api/CinemaService/Configuration/VersioningConfiguration.cs
@@ -0,0 +1,32 @@
+﻿namespace CinemaService.Api.Configuration;
+
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// Versioning configuration
+/// </summary>
+public static class VersioningConfiguration
+{
+    /// <summary>
+    /// Adds version support for API
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <returns>The service collection</returns>
+    public static IServiceCollection AddAppVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ReportApiVersions = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+        });
+        
+        services.AddVersionedApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+        });
+
+        return services;
+    }
+}
