67e1d12857d4137a30a9fed5d257e04f2aecdbac
Roman Vetrov <ash.tree4314@gmail.com>
2023-04-15 16:08:07 +0300

    Add HealthChekcs

---
+++ Systems/Api/CinemaService/Configuration/HealthCheckConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace CinemaService.Api.Configuration;
+
+using System;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using CinemaService.Common;
+/// <summary>
+/// Health checks configuration
+/// </summary>
+public static class HealthCheckConfiguration
+{
+	/// <summary>
+	/// Adds health check for API to <paramref name="services"/>
+	/// </summary>
+	/// <param name="services">Service collection</param>
+	/// <returns>The service collection</returns>
+	public static IServiceCollection AddAppHealthChecks(this IServiceCollection services)
+	{
+		services.AddHealthChecks()
+			.AddCheck<SelfHealthCheck>("CinemaService");
+		return services;
+	}
+    /// <summary>
+    /// Adds and configures health check endpoint
+    /// </summary>
+    /// <param name="app"></param>
+    public static void UseAppHealthChecks(this WebApplication app)
+	{
+		app.MapHealthChecks("/health");
+		//app.MapHealthChecksUI();
+		app.MapHealthChecks("/health/detail", new HealthCheckOptions
+		{
+			ResponseWriter = HealthCheckHelper.WriteHealthCheckResponseAsync,
+			AllowCachingResponses = false,
+		});
+	}
+}
---
+++ Systems/Api/CinemaService/Configuration/HealthChecks/SelfHealthCheck.cs
@@ -0,0 +1,15 @@
+﻿namespace CinemaService.Api.Configuration;
+
+using System;
+using System.Reflection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+public class SelfHealthCheck : IHealthCheck
+{
+	public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+	{
+		var assembly = Assembly.Load("CinemaService");
+		var versionNumber = assembly.GetName().Version;
+
+		return Task.FromResult(HealthCheckResult.Healthy(description: $"Build {versionNumber}"));
+	}
+}
